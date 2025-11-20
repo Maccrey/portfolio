5fa0a882336112b32da7d94631ebdf2b448e7ee9
Maccrey <m01071630214@gmail.com>
2025-11-20 11:46:18 +0900

    chore: deploy static export

--- web-portfolio/src/hooks/useLocaleChange.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-
-export function useLocaleChange(callback: () => void) {
-  useEffect(() => {
-    document.addEventListener("maccrey-locale-change", callback);
-    return () => document.removeEventListener("maccrey-locale-change", callback);
-  }, [callback]);
-}
--- web-portfolio/vitest.setup.ts
+++
@@ -1,7 +0,0 @@
-import "@testing-library/jest-dom";
-import React from "react";
-
-// Next.js compiles pages/components assuming React is in scope when using the classic runtime.
-// Provide a global for the Vitest environment so client components can render without additional imports.
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-(globalThis as any).React = React;
