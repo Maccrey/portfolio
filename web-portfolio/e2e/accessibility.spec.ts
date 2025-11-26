1b7e3e9cc8922303b3dc890a96a66dff6a201566
Maccrey <m01071630214@gmail.com>
2025-11-26 14:12:18 +0900

    Fixed accessibility test failure on CI

--- web-portfolio/e2e/accessibility.spec.ts
+++ web-portfolio/e2e/accessibility.spec.ts
@@ -9,7 +9,7 @@ test.describe("접근성 체크", () => {
       await page.goto(route);
       const accessibilityScanResults = await new AxeBuilder({ page })
         .withTags(["wcag2a", "wcag2aa"])
-        .exclude(["#nextjs-portal", "[data-nextjs-toast]"])
+        .exclude(["#nextjs-portal", "nextjs-portal", "[data-nextjs-toast]", ".nextjs-toast-errors-parent", ".nextjs-toast"])
         .analyze();
       expect(accessibilityScanResults.violations).toEqual([]);
     });
